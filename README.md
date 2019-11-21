d9dd6710afbb31c9f0d83586c59f8ed85514f44c
Tom Hendra <tomhendra@TomHs-MacBook-Pro.local>
2019-11-21 19:08:15 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gridful
+
+[Gridsome](https://gridsome.org/) [Contentful](https://www.contentful.com) Blog Starter
+
+Check out the demo at [gridful.netlify.com](https://gridful.netlify.com)
+
+Derived from [gatsby-contentful-starter](https://github.com/contentful-userland/gatsby-contentful-starter) with gratitude
---
+++ src/components/Articles.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="section">
+    <ul class="article-list">
+    <marquee scrollamount="50">
+        <img :src="`${$static.header.edges[0].node.mainImage.file.url.src}`">
+    </marquee>
+    <h1>The many faces of Sylvester Stallone</h1>
+    <marquee scrollamount="10" direction="up">
+      <img :src="`${$static.header.edges[1].node.mainImage.file.url.src}`">
+    </marquee>
+    <marquee scrollamount="50" direction="right">
+      <img :src="`${$static.header.edges[2].node.mainImage.file.url.src}`">
+    </marquee>
+    </ul>
+  </div>
+</template>
+<static-query>
+  query HomeQuery {
+    posts: allContentfulBlogPost {
+      edges {
+        node {
+          title
+          description
+          slug
+          publishDate(format: "MMMM Do, YYYY")
+          tags
+          heroImage {
+            path
+            slug
+            file {
+              fileName
+              url
+            }
+          }
+          description
+        }
+      }
+    }
+    header: 
+  allContentfulPageHeader {
+    edges {
+      node {
+        title
+        subHeading
+        mainImage {
+          path
+          slug
+          file {
+            url
+            fileName
+          }
+        }
+        
+      }
+    }
+  }
+}
+
+</static-query>
+<script>
+
+export default {
+  components: {
+  },
+};
+</script>
+
+<style scoped>
+  .section {
+    display: flex;
+    align-items: center;
+    height: 500px;
+  }
+  ul {
+    height: 100%; 
+    display: flex;
+    align-items: center;
+  }
+  h1 {
+    font-weight: bold
+  }
+</style>
